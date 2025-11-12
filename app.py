f24ae5a834ce7c3769fb0356f352d5e6a2e66815
Marco Andrew D. Zafra <zaframarco123@gmail.com>
2025-11-12 17:47:04 +0800

    Revert "config.py done with the additional lines in app.py"

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template
-from dotenv import load_dotenv
-from config import Config
 
 app = Flask(__name__)
-app.config.from_object(Config)
 
 @app.route('/')
 def home():
